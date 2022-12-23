b8879c8f893a562a0194be2fd8f1837d2a8f5635
Daniel Phan <sc20dkvp@leeds.ac.uk>
2022-12-23 13:20:33 +0000

    loadcubemap

--- cw2/main/loadTexture.cpp
+++ cw2/main/loadTexture.cpp
@@ -5,6 +5,7 @@
 
 //#include <unistd.h>
 #include <io.h>
+#include <string>
 
 #include "../support/error.hpp"
 
@@ -41,42 +42,6 @@ unsigned int loadTexture(char const * path) {
     return textureID;
 }
 
-unsigned int loadCubeMap(std::vector<std::string> faces) {
-
-    unsigned int textureID;
-    glGenTextures(1, &textureID);
-
-    int width, height, nrComponents;
-    for (unsigned int i = 0; i < faces.size(); i++)
-    {
-        unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrComponents, 0);
-        GLenum format = 0;
-        if (nrComponents == 1)
-            format = GL_RED;
-        else if (nrComponents == 3)
-            format = GL_RGB;
-        else if (nrComponents == 4)
-            format = GL_RGBA;
-        if (data)
-        {
-            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-            stbi_image_free(data);
-        }
-        else
-        {
-            printf("Cubemap texture failed to load at: %s\n", faces[i].c_str());
-        }
-    }
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-
-
-    return textureID;
-}
-
 
 unsigned int createTexture(int width, int height, uint8_t * pixels) {
 	int CHANNEL_NUM = 3;
@@ -85,12 +50,20 @@ unsigned int createTexture(int width, int height, uint8_t * pixels) {
 	//std::string number = std::to_string(i);
     int fullSize = width * height * CHANNEL_NUM;
     uint8_t* reversedPixels = new uint8_t[fullSize];
-    //for (int i = 0; i < windowWidth * windowHeight * 3; i++)
+    for (int i = 0; i < height ; i++) {
+        for (int l = 0; l < width * CHANNEL_NUM; l += 1) {
+            reversedPixels[(height - i-1)*width* CHANNEL_NUM  + l] = pixels[(i)*width* CHANNEL_NUM + l];
+            //reversedPixels[(height - i-1)*width* CHANNEL_NUM  + l] = pixels[(i)*width* CHANNEL_NUM + l];
+            //reversedPixels[(height - i-1)*width* CHANNEL_NUM  + l + 1] = pixels[(i)*width* CHANNEL_NUM + l + 1];
+            //reversedPixels[(height - i-1)*width* CHANNEL_NUM  + l + 2] = pixels[(i)*width* CHANNEL_NUM + l + 2];
+        }
+    }
+    /*
     for (int i = 0; i < fullSize; i += 3) {
         reversedPixels[fullSize - i - 3] = pixels[i];
         reversedPixels[fullSize - i - 2] = pixels[i+1];
         reversedPixels[fullSize - i -1] = pixels[i+2];
-    }
+    }*/
 
     int c = 1;
     //char* fileName = "screenshot"+ (char*)c+".png";
